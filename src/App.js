c19f71d7d0f65e4de03a3dc54995b16e6607f3d7
Michael Hermenault <you@example.com>
2020-12-27 01:07:49 -0500

    WIP: fetch results from backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { CrawlGraph } from "./CrawlGraph";
+import nickel from "./nickel.webp"; // Tell webpack this JS file uses this image
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={nickel} alt="Nickel" />
+      <CrawlGraph />;
     </div>
   );
 }
