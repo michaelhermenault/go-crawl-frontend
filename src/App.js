cae004f58bc18bc5ad2ed7cc47e161336cfd3ce4
Michael Hermenault <you@example.com>
2021-01-01 22:30:04 -0500

    Move main logic up to App class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,150 @@
 import "./App.css";
 
 import React from "react";
-import { CrawlGraph, sanitizeURL } from "./CrawlGraph";
+import { CrawlGraph } from "./CrawlGraph";
 import { CrawlForm } from "./CrawlForm";
+import axios from "axios";
+
+const pollingPeriod = 2000;
+const animationPeriod = 50;
+const animationTimeScale = 3;
+
+function createImageContainerForDomain(domain) {
+  let img = new Image();
+  let imgContainer = { imgLoaded: false, image: img };
+
+  img.onload = function () {
+    imgContainer.imgLoaded = true;
+  };
+  img.src = "https://www.google.com/s2/favicons?domain=" + domain;
+
+  return imgContainer;
+}
+
+function getDomain(url) {
+  return url.replace(/https?:\/\//, "").split(/[/?#]/)[0];
+}
+
+function sanitizeURL(url) {
+  return url.replace(/(^\w+:|^)\/\/(www\.)?/, "");
+}
+
+function allBeforeTime(links, targetTime) {
+  let start = 0;
+  let end = links.length - 1;
+  let ans = -1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    // Move to the left side if the target is smaller
+    if (links[mid].timeFound >= targetTime) {
+      end = mid - 1;
+    }
+
+    // Move right side
+    else {
+      ans = mid;
+      start = mid + 1;
+    }
+  }
+
+  return ans;
+}
 
 class App extends React.Component {
+  componentDidMount() {
+    this.existingNodes = new Set().add(this.startingURL);
+    this.exisitingEdges = new Set();
+    this.babyLinks = [];
+  }
+
+  startCrawl(startingURL) {
+    axios
+      .post("https://crawl.hermenault.dev/crawl/", {
+        url: "https://" + startingURL,
+      })
+      .then((res) => {
+        this.fetchCrawlResults(res.data.resultsURL);
+        setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
+        this.startTime = Date.now() + 2000;
+      });
+  }
+
+  fetchCrawlResults(resultsURL) {
+    axios.get(resultsURL).then((res) => {
+      if (
+        res.data.hasOwnProperty("_links") &&
+        res.data._links.hasOwnProperty("next")
+      ) {
+        // Fetch the next set of data.
+        setTimeout(
+          this.fetchCrawlResults.bind(this, res.data._links.next.href),
+          pollingPeriod
+        );
+      } else {
+      }
+      this.queueCrawlResults(res.data.edges);
+    });
+  }
+
+  queueCrawlResults(newResults) {
+    for (const result of newResults) {
+      for (const child of result.Children) {
+        let imgContainer = createImageContainerForDomain(getDomain(child));
+
+        this.babyLinks.push({
+          source: sanitizeURL(result.Parent),
+          target: sanitizeURL(child),
+          depth: result.Depth,
+          targetImageContainer: imgContainer,
+          // Convert from nanoseconds to milliseconds
+          timeFound: (animationTimeScale * result.TimeFound) / 1000000,
+        });
+      }
+    }
+  }
+
+  displayCrawlResults() {
+    if (this.babyLinks.length === 0) {
+      setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
+      return;
+    }
+
+    const lastBabyLinkToDisplay = allBeforeTime(
+      this.babyLinks,
+      Date.now() - this.startTime
+    );
+
+    if (lastBabyLinkToDisplay < 0) {
+      setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
+      return;
+    }
+
+    let newNodes = [];
+    let newLinks = [];
+    for (let i = 0; i < lastBabyLinkToDisplay; i++) {
+      if (!this.existingNodes.has(this.babyLinks[i].target)) {
+        newNodes.push({
+          id: this.babyLinks[i].target,
+          depth: this.babyLinks[i].depth,
+          imageContainer: this.babyLinks[i].targetImageContainer,
+        });
+        this.existingNodes.add(this.babyLinks[i].target);
+      }
+      this.exisitingEdges.add(this.babyLinks[i]);
+      newLinks.push(this.babyLinks[i]);
+    }
+    this.setState({
+      graphData: {
+        nodes: this.state.graphData.nodes.concat(newNodes),
+        links: this.state.graphData.links.concat(newLinks),
+      },
+    });
+
+    this.babyLinks = this.babyLinks.slice(lastBabyLinkToDisplay);
+    setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
+  }
+
   constructor(props) {
     super(props);
     this.crawlURLCallback = this.crawlURLCallback.bind(this);
@@ -14,7 +154,21 @@ class App extends React.Component {
   }
   crawlURLCallback(url) {
     this.startingURL = sanitizeURL(url);
-    this.setState({ submitState: "VALIDATING" });
+    this.startCrawl(this.startingURL);
+    this.setState({
+      submitState: "VALIDATING",
+      graphData: {
+        nodes: [
+          {
+            id: this.startingURL,
+            imageContainer: createImageContainerForDomain(
+              getDomain(this.startingURL)
+            ),
+          },
+        ],
+        links: [],
+      },
+    });
   }
   render() {
     return (
@@ -23,7 +177,7 @@ class App extends React.Component {
           <CrawlForm crawlURLCallback={this.crawlURLCallback} />
         )}
         {this.state.submitState === "VALIDATING" && (
-          <CrawlGraph startingURL={this.startingURL} />
+          <CrawlGraph graphData={this.state.graphData} />
         )}
       </div>
     );
--- src/CrawlGraph.js
+++ src/CrawlGraph.js
@@ -1,55 +1,9 @@
 import ForceGraph2D from "react-force-graph-2d";
 import React from "react";
-import axios from "axios";
 
-const pollingPeriod = 2000;
-const animationPeriod = 50;
 const maxNodeLabelLength = 30;
 const defaultIconSize = 20;
 const scaleForLabels = 1.7;
-const animationTimeScale = 3;
-
-function createImageContainerForDomain(domain) {
-  let img = new Image();
-  let imgContainer = { imgLoaded: false, image: img };
-
-  img.onload = function () {
-    imgContainer.imgLoaded = true;
-  };
-  img.src = "https://www.google.com/s2/favicons?domain=" + domain;
-
-  return imgContainer;
-}
-
-function allBeforeTime(links, targetTime) {
-  let start = 0;
-  let end = links.length - 1;
-  let ans = -1;
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-
-    // Move to the left side if the target is smaller
-    if (links[mid].timeFound >= targetTime) {
-      end = mid - 1;
-    }
-
-    // Move right side
-    else {
-      ans = mid;
-      start = mid + 1;
-    }
-  }
-
-  return ans;
-}
-
-function getDomain(url) {
-  return url.replace(/https?:\/\//, "").split(/[/?#]/)[0];
-}
-
-function sanitizeURL(url) {
-  return url.replace(/(^\w+:|^)\/\/(www\.)?/, "");
-}
 
 function drawIcon(imageContainer, ctx, xPos, yPos, iconSize) {
   if (imageContainer !== undefined && imageContainer.imgLoaded) {
@@ -58,122 +12,11 @@ function drawIcon(imageContainer, ctx, xPos, yPos, iconSize) {
 }
 
 class CrawlGraph extends React.Component {
-  componentDidMount() {
-    this.existingNodes = new Set().add(this.props.startingURL);
-    this.exisitingEdges = new Set();
-    this.babyLinks = [];
-    this.startCrawl(this.props.startingURL);
-  }
-
-  startCrawl(startingURL) {
-    axios
-      .post("https://crawl.hermenault.dev/crawl/", {
-        url: "https://" + startingURL,
-      })
-      .then((res) => {
-        this.fetchCrawlResults(res.data.resultsURL);
-        setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
-        this.startTime = Date.now() + 2000;
-      });
-  }
-
-  fetchCrawlResults(resultsURL) {
-    axios.get(resultsURL).then((res) => {
-      if (
-        res.data.hasOwnProperty("_links") &&
-        res.data._links.hasOwnProperty("next")
-      ) {
-        // Fetch the next set of data.
-        setTimeout(
-          this.fetchCrawlResults.bind(this, res.data._links.next.href),
-          pollingPeriod
-        );
-      } else {
-      }
-      this.queueCrawlResults(res.data.edges);
-    });
-  }
-
-  queueCrawlResults(newResults) {
-    for (const result of newResults) {
-      for (const child of result.Children) {
-        let imgContainer = createImageContainerForDomain(getDomain(child));
-
-        this.babyLinks.push({
-          source: sanitizeURL(result.Parent),
-          target: sanitizeURL(child),
-          depth: result.Depth,
-          targetImageContainer: imgContainer,
-          // Convert from nanoseconds to milliseconds
-          timeFound: (animationTimeScale * result.TimeFound) / 1000000,
-        });
-      }
-    }
-  }
-
-  displayCrawlResults() {
-    if (this.babyLinks.length === 0) {
-      setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
-      return;
-    }
-
-    const lastBabyLinkToDisplay = allBeforeTime(
-      this.babyLinks,
-      Date.now() - this.startTime
-    );
-
-    if (lastBabyLinkToDisplay < 0) {
-      setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
-      return;
-    }
-
-    let newNodes = [];
-    let newLinks = [];
-    for (let i = 0; i < lastBabyLinkToDisplay; i++) {
-      if (!this.existingNodes.has(this.babyLinks[i].target)) {
-        newNodes.push({
-          id: this.babyLinks[i].target,
-          depth: this.babyLinks[i].depth,
-          imageContainer: this.babyLinks[i].targetImageContainer,
-        });
-        this.existingNodes.add(this.babyLinks[i].target);
-      }
-      this.exisitingEdges.add(this.babyLinks[i]);
-      newLinks.push(this.babyLinks[i]);
-    }
-    this.setState({
-      graphData: {
-        nodes: this.state.graphData.nodes.concat(newNodes),
-        links: this.state.graphData.links.concat(newLinks),
-      },
-    });
-
-    this.babyLinks = this.babyLinks.slice(lastBabyLinkToDisplay);
-    setTimeout(this.displayCrawlResults.bind(this), animationPeriod);
-  }
-
-  constructor(props) {
-    super(props);
-    console.log(props);
-    this.state = {
-      graphData: {
-        nodes: [
-          {
-            id: this.props.startingURL,
-            imageContainer: createImageContainerForDomain(
-              getDomain(this.props.startingURL)
-            ),
-          },
-        ],
-        links: [],
-      },
-    };
-  }
   render() {
     return (
       <ForceGraph2D
         enableNodeDrag={false}
-        graphData={this.state.graphData}
+        graphData={this.props.graphData}
         backgroundColor="white"
         nodeCanvasObject={(node, ctx, globalScale) => {
           const drawnSize = defaultIconSize / globalScale;
@@ -219,4 +62,4 @@ class CrawlGraph extends React.Component {
   }
 }
 
-export { CrawlGraph, sanitizeURL };
+export { CrawlGraph };
